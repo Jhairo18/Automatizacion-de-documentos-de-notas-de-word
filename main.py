88132128b0946872ec2c3a572d07ca0abc4bab95
Jhairo <77685103+Jhairo18@users.noreply.github.com>
2025-03-17 17:48:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,6 @@ import pandas as pd
 from datetime import datetime
 from docxtpl import DocxTemplate
 
-
-# El presente programa consiste que a partir de un documento plantilla (plantilla.docx) tenemos una 
-# carta que dara las notas de los alumnos, pero tenemos diferentes alumnos y diferentes notas
-# Tenemos estos datos en un archivo excel. El programa consiste en llenar los datos cambiantes de
-# la plantilla las cuales estaran referenciadas como {{}} con los datos excel.
-
-
-# La libreria docxtpl sirve para poder trabajar los archivos de word, además que con el render podemos
-# reemplazar algunas palabras escritas en el siguiente formato {{palabra}}, y con el save pues guardamos 
-# los archivos.
-# En pandas usamos read_excel para leer los archivos excel de python, además  de usar el metodo iterrows 
-# que me permite como el mismo nombre dice iterar las filas, y asi obtener por una parte los indices
-# y también las filas las cuales estaran representadas por una de pdSeries.
-# Con datetime obtenemos la fecha actualizada, poniendo nuestro formato de tiempo usando el metodo
-# strftime 
-
-
 doc = DocxTemplate("plantilla.docx")
 
 nombre = "Jhairo Yurivilca"
